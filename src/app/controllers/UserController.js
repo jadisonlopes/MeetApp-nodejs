0594f03abf7c623a9fb4cd69698b9d837347aef8
Jadison Lopes <jadison.lopes.s@gmail.com>
2019-07-03 02:59:36 -0300

    chore: Update user

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -13,7 +13,28 @@ class UserController {
   }
 
   async update(req, res) {
-    return res.json({ user: 'Ok' });
+    const user = await User.findByPk(req.userId);
+    const { email, oldPassword } = req.body;
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match.' });
+    }
+
+    const { id, name } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
   }
 }
 
