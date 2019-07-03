ad36e1b15552b9bbe035b9e45113685fda954c6a
Jadison Lopes <jadison.lopes.s@gmail.com>
2019-07-02 23:15:52 -0300

    chore: Check of user exists

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,6 +2,12 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists.' });
+    }
+
     const user = await User.create(req.body);
     return res.json(user);
   }
