dff28390bfa1628b4a20d3802b4a1e6a7806b6d1
Jadison Lopes <jadison.lopes.s@gmail.com>
2019-07-03 02:33:39 -0300

    feature: Authentication token

---
+++ src/app/middleware/auth.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided.' });
+  }
+
+  const [, token] = authHeader.split(' ');
+  const { secret } = authConfig;
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, secret);
+
+    req.userId = decoded.id;
+
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Token invalid' });
+  }
+};
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,15 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import auth from './app/middleware/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
+
+routes.use(auth);
+
+routes.put('/users', UserController.update);
 
 export default routes;
