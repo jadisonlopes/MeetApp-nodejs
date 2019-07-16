2e13809c57807cc59a0e046953548db5bac7177b
Jadison Lopes <jadison.lopes.s@gmail.com>
2019-07-16 01:29:39 -0300

    chore: Validation data and date

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -1,7 +1,29 @@
+import * as Yup from 'yup';
+import { parseISO, startOfHour, isBefore } from 'date-fns';
+
 import Meetup from '../models/Meetup';
 
 class MeetupController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      description: Yup.string().required(),
+      location: Yup.string().required(),
+      date: Yup.date().required(),
+      file_id: Yup.number().required(),
+      user_id: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
+    const hourStart = startOfHour(parseISO(req.body.date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted.' });
+    }
+
     const meetup = await Meetup.create(req.body);
 
     return res.json(meetup);
